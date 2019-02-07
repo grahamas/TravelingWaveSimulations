b609c593c9c3f92fea2a8bf0b6dced200074519f
Graham Smith <grahamas@gmail.com>
2019-02-07 09:53:12 -0600

    Fix bug in MatchExample fitting function.

--- src/target.jl
+++ src/target.jl
@@ -25,16 +25,17 @@ function MatchExample(; file_name::String="")
 	MatchExample(wave, x, t)
 end
 
-function (p::MatchExample{T})(soln::AbstractArray{T}, x_dxs::AbstractArray{Int,1}, pop_dxs::AbstractArray{Int,1}, t_dxs::AbstractArray{Int,1}) where {T}
-	sum((soln[x_dxs, pop_dxs, t_dxs] .- p.data) .^ 2)
+function (p::MatchExample{T})(soln::AbstractArray{T}) where T#, x_dxs::AbstractArray{Int,1}, pop_dxs::AbstractArray{Int,1}, t_dxs::AbstractArray{Int,1}) where {T}
+	res = sum((soln .- p.data) .^ 2)
+	return res
 end
 
 function target_loss(fn::MatchExample{T}, model::WCMSpatial1D{T}, solver::Solver{T}) where T
 	t_target, x_target = fn.t, fn.x
 	t_dxs = subsampling_time_idxs(t_target, solver)
 	x_dxs = subsampling_space_idxs(x_target, model, solver)
-	pop_dxs = [1]
-	(soln) -> fn(soln, x_dxs, pop_dxs, t_dxs)
+	pop_dxs = 1
+	(soln) -> (fn(soln[x_dxs, pop_dxs, t_dxs]))
 end
 
 #endregion
