fae1fa648d75b1416dcf0b41583b41345eb83239
Graham Smith <grahamas@gmail.com>
2019-05-05 13:49:23 -0500

    Macro to allow scalar param specification

--- scripts/modifications/connEIrange1to50.jl
+++ scripts/modifications/connEIrange1to50.jl
@@ -1,3 +1,2 @@
 EI_range = -(1.0:0.1:50.0)
-modifications = [Dict(:amplitude => [16.0 EI;
-                                     27.0 -4.0]) for EI in EI_range]
+modifications = [Dict(:amplitudeEI =>  EI) for EI in EI_range]
--- src/examples.jl
+++ src/examples.jl
@@ -2,6 +2,7 @@ module Examples
 
 using WilsonCowanModel
 using Simulation73
+using MacroTools
 import DifferentialEquations: Euler
 
 function get_example(example_name)
@@ -12,7 +13,43 @@ function get_example(example_name)
     return examples[example_name]
 end
 
-function neuman_line(;
+"""
+Parse a function with vector kwargs into function taking corresponding EI scalar arguments
+"""
+macro EI(fn_def)
+  fn_dct = MacroTools.splitdef(fn_def)
+  new_kwargs = map(fn_dct[:kwargs]) do kwarg
+    EI_parsekwarg(MacroTools.splitarg(kwarg)...) .|> (x) -> MacroTools.combinearg(x...)
+  end
+  fn_dct[:kwargs] = cat(new_kwargs..., dims=1)
+  MacroTools.combinedef(fn_dct) |> esc
+end
+
+function EI_parsekwarg(name::Symbol, type::Symbol, splat::Bool, default::Expr)
+  if @capture(default, [E_, I_])
+    [
+      (Symbol(name,:E), :Any, false, E),
+      (Symbol(name,:I), :Any, false, I),
+      (name, type, splat, :([$(Symbol(name,:E)), $(Symbol(name,:I))]))
+    ]
+  elseif @capture(default, [EE_ EI_; IE_ II_])
+    [
+      (Symbol(name,:EE), :Any, false, EE),
+      (Symbol(name,:IE), :Any, false, IE),
+      (Symbol(name,:EI), :Any, false, EI),
+      (Symbol(name,:II), :Any, false, II),
+      (name, type, splat, :([$(Symbol(name,:EE)) $(Symbol(name,:EI)); $(Symbol(name,:IE)) $(Symbol(name,:II))]))
+    ]
+  else
+    [(name, type, splat, default)]
+  end
+end
+
+function EI_parsekwarg(name::Symbol, type::Symbol, splat::Bool, default::Any)
+  return [(name, type, splat, default)]
+end
+
+@EI function neuman_line(;
         α = [1.1, 1.0],
         β = [1.1, 1.1],
         τ = [0.1, 0.18],
@@ -71,7 +108,7 @@ function neuman_line(;
     return simulation
 end
 
-function neuman_square(;
+@EI function neuman_square(;
     α = [1.1, 1.0],
     β = [1.1, 1.1],
     τ = [0.1, 0.18],
