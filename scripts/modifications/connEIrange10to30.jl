0b1ecdd240c6b2094cb9fab43e41d4088a15969b
Graham Smith <grahamas@gmail.com>
2019-05-06 10:15:56 -0500

    Connectivity sweep modifications

--- scripts/modifications/connEIrange10to30.jl
+++
@@ -1,3 +0,0 @@
-EI_range = -(10.0:0.1:30.0)
-modifications = [Dict(:amplitude => [16.0 EI;
-                                     27.0 -4.0]) for EI in EI_range]
--- scripts/modifications/connEIrange1to10.jl
+++
@@ -1,3 +0,0 @@
-EI_range = -(1.0:0.1:10.0)
-modifications = [Dict(:amplitude => [16.0 EI;
-                                     27.0 -4.0]) for EI in EI_range]
