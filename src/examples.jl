c4a0aeff1f91f7ba917669f3a14739631dfb9350
Graham Smith <grahamas@gmail.com>
2019-05-07 10:11:06 -0500

    Macro to automatically define example

--- src/examples.jl
+++ src/examples.jl
@@ -49,6 +49,20 @@ function EI_parsekwarg(name::Symbol, type::Symbol, splat::Bool, default::Any)
   return [(name, type, splat, default)]
 end
 
+macro kw_example(fn_expr)
+  fn_dct = MacroTools.splitdef(fn_expr)
+  # postwalk processes leaves first, so they can be used to set defaults in larger kwargs
+  fn_dct[:body] = MacroTools.postwalk(fn_dct[:body]) do x
+    if isa(x, Expr) && (x.head == :kw)
+      push!(fn_dct[:kwargs], Expr(:kw, x.args[1], x.args[2]))
+      Expr(:kw, x.args[1], x.args[1]) # INTENTIONALLY set default to same symbol
+    else
+      x
+    end
+  end
+  MacroTools.combinedef(fn_dct) |> esc
+end
+
 @EI function neuman_line(;
         α = [1.1, 1.0],
         β = [1.1, 1.1],
