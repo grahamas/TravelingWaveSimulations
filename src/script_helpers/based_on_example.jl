d61c29251e7ed97ebf74d1368e1c18d97c819f56
Graham Smith <grahams@uchicago.edu>
2019-07-15 16:59:44 -0500

    [wip] trying to parallelize

--- src/script_helpers/based_on_example.jl
+++ src/script_helpers/based_on_example.jl
@@ -1,7 +1,7 @@
 
 export based_on_example
 
-function based_on_example(; data_root::AbstractString=nothing, no_save_raw::Bool=false,
+function based_on_example(; data_root::AbstractString=datadir(), no_save_raw::Bool=false,
         example_name::AbstractString=nothing,
         modifications::AbstractArray=[],
         plot_specs::AbstractArray=[])
@@ -24,18 +24,39 @@ function based_on_example(; data_root::AbstractString=nothing, no_save_raw::Bool
     end
 
     example = get_example(example_name)
-    for modification in modifications
-        simulation = example(; modification...)
-        execution = execute(simulation)
-        mod_name = savename(modification; allowedtypes=(Real,String,Symbol,AbstractArray), connector=";")
-        if mod_name == ""
-            mod_name = "no_mod"
+    if length(modifications) > 2
+        plot_lock = Threads.Mutex()
+        @warn "Parallelizing with $(Threads.nthreads()) threads."
+        Threads.@threads for modification in modifications
+            simulation = example(; modification...)
+            execution = execute(simulation)
+            mod_name = savename(modification; allowedtypes=(Real,String,Symbol,AbstractArray), connector=";")
+            if mod_name == ""
+                mod_name = "no_mod"
+            end
+            if !no_save_raw
+                execution_dict = @dict execution
+                @warn("not currently saving raw")
+                #@tagsave("$(joinpath(sim_output_path, mod_name)).bson", execution_dict, true)
+            end
+            Threads.lock(plot_lock)
+            plot_and_save.(plot_specs, Ref(execution), plots_path, mod_name)
+            Threads.unlock(plot_lock)
         end
-        if !no_save_raw
-            execution_dict = @dict execution
-            @warn("not currently saving raw")
-            #@tagsave("$(joinpath(sim_output_path, mod_name)).bson", execution_dict, true)
+    else
+        for modification in modifications
+            simulation = example(; modification...)
+            execution = execute(simulation)
+            mod_name = savename(modification; allowedtypes=(Real,String,Symbol,AbstractArray), connector=";")
+            if mod_name == ""
+                mod_name = "no_mod"
+            end
+            if !no_save_raw
+                execution_dict = @dict execution
+                @warn("not currently saving raw")
+                #@tagsave("$(joinpath(sim_output_path, mod_name)).bson", execution_dict, true)
+            end
+            plot_and_save.(plot_specs, Ref(execution), plots_path, mod_name)
         end
-        plot_and_save.(plot_specs, Ref(execution), plots_path, mod_name)
     end
 end
