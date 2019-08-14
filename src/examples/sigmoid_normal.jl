042162f6b7d4a04cbdf5783c6986cc693a1b78ee
Graham Smith <grahamas@gmail.com>
2019-08-14 11:14:49 -0500

    [wip] moved space from model to sim

--- src/examples/sigmoid_normal.jl
+++ src/examples/sigmoid_normal.jl
@@ -1,14 +1,13 @@
-@EI_kw_example function example(N_ARR=2,N_CDT=2,P=2; spread_scale=1.0, amplitude_scale=1.0, 
-                                                     SNR_scale=80.0, auto=25.0, 
+@EI_kw_example function example(N_ARR=2,N_CDT=2,P=2; spread_scale=1.0, amplitude_scale=1.0,
+                                                     SNR_scale=80.0, auto=25.0,
                                                      ca_ratio=(27.0/25.0), cross=(ca_ratio*auto),
                                                      inh_scale=1.0)
   simulation = Simulation(;
-    model = WCMSpatial{Float64,N_ARR,N_CDT,P}(;
+    model = WCMSpatial{Float64,N_CDT,P}(;
       pop_names = ["E", "I"],
       α = [1.0, 1.0],
       β = [1.0, 1.0],
       τ = [10.0, 10.0],
-      space = CompactLattice{Float64,N_ARR}(; n_points=(35,35), extent=(350.0,350.0)),
       nonlinearity = pops(SigmoidNonlinearity{Float64};
         a = [1.2, 1.0],
         θ = [2.6, 8.0]),
@@ -27,12 +26,10 @@
                     (cross,cross) (auto,auto)] .|> (tup) -> map((x) -> x * spread_scale, tup)
           )
       ),
-    solver = Solver{Float64}(;
-        stop_time = 100.0,
-        dt = 1.0,
-        space_save_every=1,
-        time_save_every=1,
-        algorithm=Euler()
+      space = CompactLattice{Float64,N_ARR}(; n_points=(35,35), extent=(350.0,350.0)),
+      tspan = (0.0,100.0),
+      dt = 1.0,
+      algorithm=Euler()
       )
   )
 end
