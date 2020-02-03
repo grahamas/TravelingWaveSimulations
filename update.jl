3503d5dc79abcb91ef0d7fd517a8c5bbca229ffa
Graham Smith <grahams@uchicago.edu>
2020-02-03 08:48:55 -0600

    code to update manifests

---
+++ update.jl
@@ -0,0 +1,34 @@
+using Pkg
+
+rel_path(dirs...) = joinpath(@__DIR__, dirs...)
+git_pull(path) = run(`git -C $(path) pull`)
+
+s73_path = rel_path("..", "Simulation73")
+nm_path = rel_path("..", "NeuralModels")
+wcm_path = rel_path("..", "WilsonCowanModel")
+tws_path = rel_path(".")
+
+git_pull.([s73_path, nm_path, wcm_path, tws_path])
+
+Pkg.activate(s73_path)
+Pkg.update()
+Pkg.activate(joinpath(s73_path, "test"))
+Pkg.update()
+
+Pkg.activate(nm_path)
+Pkg.update()
+Pkg.activate(joinpath(nm_path, "test"))
+Pkg.update()
+
+Pkg.activate(wcm_path)
+Pkg.update()
+Pkg.activate(joinpath(wcm_path, "test"))
+Pkg.update()
+
+Pkg.activate(tws_path)
+Pkg.update()
+Pkg.activate(joinpath(tws_path, "test"))
+Pkg.update()
+
+Pkg.instantiate()
+Pkg.build()
