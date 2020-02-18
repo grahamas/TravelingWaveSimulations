3de10690db3cf00aaf69504ed75db3f54c80390a
Graham Smith <grahamas@gmail.com>
2020-02-18 21:46:43 +0000

    Moving away from zeroed sigmoid nonlinearities

--- src/examples/dos_effectively_sigmoid.jl
+++ src/examples/dos_effectively_sigmoid.jl
@@ -1,12 +1,13 @@
 ABS_STOP=300.0
-dos_example = TravelingWaveSimulations.@EI_kw_example function example(N_ARR=2,N_CDT=2,P=2; SNR_scale=80.0, stop_time=ABS_STOP,
-                                                     Aee=24.0, See=25.0,
-                                                     Aii=4.0, Sii=27.0,
-                                                     Aie=27.0, Sie=25.0,
-                                                     Aei=18.2, Sei=25.0,
+TravelingWaveSimulations.@EI_kw_example function example(N_ARR=2,N_CDT=2,P=2; SNR_scale=80.0, stop_time=ABS_STOP,
+                                                     Aee=70.0, See=25.0,
+                                                     Aii=2.0, Sii=27.0,
+                                                     Aie=35.0, Sie=25.0,
+                                                     Aei=70.0, Sei=27.0,
                                                      n=256, x=700.0, 
-                                                     stim_strength=1.2,
-                                                     stim_width=28.1)
+                                                     stim_strength=6.0,
+                                                     stim_width=28.1,
+                                                     stim_duration=7.0)
   simulation = Simulation(
     WCMSpatial(;
       pop_names = ("E", "I"),
@@ -14,14 +15,14 @@ dos_example = TravelingWaveSimulations.@EI_kw_example function example(N_ARR=2,N
       β = (1.0, 1.0),
       τ = (3.0, 3.0),
       nonlinearity = pops(DifferenceOfSigmoids;
-        firing_θ = [2.6, 8.0],
+        firing_θ = [6.0, 11.4],
         firing_a = [1.2, 1.0],
-        blocking_θ = [20.0, 20.0],
+        blocking_θ = [30.0, 30.0],
         blocking_a = [1.2, 1.0]),
       stimulus = pops(SharpBumpStimulusParameter;
           strength = [stim_strength, stim_strength],
           width = [stim_width, stim_width],
-          time_windows = [[(0.0, 5.0)], [(0.0, 5.0)]],
+          time_windows = [[(0.0, stim_duration)], [(0.0, stim_duration)]],
           baseline=[0.0, 0.0]),
       connectivity = FFTParameter(pops(GaussianConnectivityParameter;
           amplitude = [Aee -Aei;
@@ -33,8 +34,8 @@ dos_example = TravelingWaveSimulations.@EI_kw_example function example(N_ARR=2,N
       space = PeriodicLattice{Float64,N_ARR}(; n_points=(n,n), extent=(x,x)),
       save_idxs = RadialSlice(),
       tspan = (0.0,stop_time),
-      dt = 1.0,
-      algorithm=Euler(),
+      dt = 0.1,
+      algorithm=Tsit5(),
       callback=DiscreteCallback(if !(save_idxs === nothing)
         (u,t,integrator) -> begin
                     sub_u = u[integrator.opts.save_idxs];
--- src/examples/sigmoid_normal_fft.jl
+++ src/examples/sigmoid_normal_fft.jl
@@ -7,15 +7,15 @@
 # ioA: inhibitory output amplitude scale
 # ioS: inhibitory output spread scale
 # iiA: inhibitory input amplitude scale
-# iiS: inhibitory input spread scale
+# iiS: inhibitory input spread scaleAbstractPopulationInteractionsParameters
 ABS_STOP = 300.0
 @EI_kw_example function example(N_ARR=2,N_CDT=2,P=2; stop_time=ABS_STOP,
-                                                     Aee=24.0, See=25.0,
-                                                     Aii=4.0, Sii=27.0,
-                                                     Aie=27.0, Sie=25.0,
-                                                     Aei=18.2, Sei=27.0,
-                                                     n=128, x=700.0, stim_strength=1.2,
-                                                     stim_width=28.1)
+                                                     Aee=70.0, See=25.0,
+                                                     Aii=2.0, Sii=27.0,
+                                                     Aie=35.0, Sie=25.0,
+                                                     Aei=70.0, Sei=27.0,
+                                                     n=128, x=700.0, stim_strength=6.0,
+                                                     stim_width=28.1, stim_duration=7.0)
   simulation = Simulation(
     WCMSpatial(;
       pop_names = ("E", "I"),
@@ -24,11 +24,11 @@ ABS_STOP = 300.0
       τ = (3.0, 3.0),
       nonlinearity = pops(SigmoidNonlinearity;
         a = [1.2, 1.0],
-        θ = [2.6, 8.0]),
+        θ = [6.0, 11.4]),
       stimulus =  pops(SharpBumpStimulusParameter;
           strength = [stim_strength,stim_strength],
           width = [stim_width, stim_width],
-          time_windows = [[(0.0, 5.0)], [(0.0, 5.0)]]),
+          time_windows = [[(0.0, stim_duration)], [(0.0, stim_duration)]]),
       connectivity = FFTParameter(pops(GaussianConnectivityParameter;
           amplitude = [Aee -Aei;
                        Aie -Aii],
@@ -39,8 +39,8 @@ ABS_STOP = 300.0
       space = PeriodicLattice{Float64,N_ARR}(; n_points=(n,n), extent=(x,x)),
       save_idxs = RadialSlice(),
       tspan = (0.0,stop_time),
-      dt = 1.0,
-      algorithm=Euler(),
+      dt = 0.1,
+      algorithm=Tsit5(),
       callback=DiscreteCallback(if !(save_idxs === nothing)
         (u,t,integrator) -> begin
                     sub_u = u[integrator.opts.save_idxs];
