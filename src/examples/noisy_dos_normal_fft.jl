8c1f4b72e2cfe5739a6f083a9697e8de28d9a5e6
Graham Smith <grahamas@gmail.com>
2020-02-25 20:06:26 +0000

    Old examples.

---
+++ src/examples/noisy_dos_normal_fft.jl
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Aee: E->E amplitude
+# See: E->E spread
+
+# caS: cross-auto spread ratio
+# caA: cross-auto amplitude ratio
+
+# ioA: inhibitory output amplitude scale
+# ioS: inhibitory output spread scale
+# iiA: inhibitory input amplitude scale
+# iiS: inhibitory input spread scale
+ABS_STOP=300.0
+@EI_kw_example function example(N_ARR=2,N_CDT=2,P=2; SNR_scale=80.0, stop_time=ABS_STOP,
+                                                     Aee=280.0, See=70.0,
+                                                     Aii=1.4, Sii=70.0,
+                                                     Aie=270.0, Sie=90.0,
+                                                     Aei=-297.0, Sei=90.0,
+                                                     n=71, x=500.0)
+    simulation = Simulation(
+    WCMSpatial(;
+      pop_names = ("E", "I"),
+      α = (1.0, 1.0),
+      β = (1.1, 1.1),
+      τ = (10.0, 10.0),
+      nonlinearity = pops(GaussianNonlinearity;
+        sd = [6.7, sqrt(3.2)],
+        θ = [18.0, 10.0]),
+      stimulus = pops(SharpBumpStimulusParameter;
+          strength = [10.0, 0.0],
+          width = [100.0, 100.0],
+          time_windows = [[(0.0, 10.0)], [(0.0, 10.0)]]),
+      connectivity = FFTParameter(pops(GaussianConnectivityParameter;
+          amplitude = [Aee -Aei;
+                       Aie -Aii],
+          spread = [(See,See) (Sei,Sei);
+                    (Sie,Sie) (Sii,Sii)]
+         ))
+      );
+      space = PeriodicLattice{Float64,N_ARR}(; n_points=(n,n), extent=(x,x)),
+      save_idxs = RadialSlice(),
+      tspan = (0.0,stop_time),
+      dt = 1.0,
+      algorithm=Euler(),
+      callback=DiscreteCallback(if !(save_idxs === nothing)
+        (u,t,integrator) -> begin
+                    sub_u = u[integrator.opts.save_idxs];
+                    (all(isapprox.(sub_u, 0.0, atol=1e-4)) || (sub_u[end] > 0.01)) && t > 5
+                end
+    else
+        (u,t,integrator) -> begin
+                    pop = population(u,1)
+                    (all(isapprox.(u, 0.0, atol=1e-4)) || (sum(pop[:,end]) / size(pop,1) > 0.01)) && t > 5
+            end
+    end, terminate!)
+  )
+end
+
