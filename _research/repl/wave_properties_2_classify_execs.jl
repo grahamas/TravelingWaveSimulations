c3404b110a220d1f176e458657b40e4cdc2a5269
Graham Smith <grahamas@gmail.com>
2020-06-03 11:41:05 -0500

    Wave properties scripts to be included from the REPL

---
+++ _research/repl/wave_properties_2_classify_execs.jl
@@ -0,0 +1,4 @@
+# execs: no_prop,  front
+
+classified_exec_no_prop = TravelingWaveSimulations.ExecutionClassifications(exec_no_prop)
+classified_exec_front = TravelingWaveSimulations.ExecutionClassifications(exec_front)
