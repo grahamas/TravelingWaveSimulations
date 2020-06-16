5d4aac76152bdbbc02e1ac845cede02a64ae3f65
Graham Smith <grahamas@gmail.com>
2020-06-16 16:33:11 -0500

    More complete parsing of mod_range input

--- _research/repl/sweep_A_blocking_2_read_data.jl
+++ _research/repl/sweep_A_blocking_2_read_data.jl
@@ -4,11 +4,16 @@ sim_name = TravelingWaveSimulations.get_sim_name(mdb.fns[1])
 
 GC.gc()
 mods = TravelingWaveSimulations.get_mods(mdb)
+@show mods
+
+is_range(x::Nothing) = false
+is_range(x::AbstractRange) = true
+is_range(x::Number) = false
 
 all_mod_names = keys(mods) |> collect
 all_mod_values = values(mods) |> collect
-varied_mods = length.(all_mod_values) .> 1
-fixed_mods = length.(all_mod_values) .== 1
+varied_mods = is_range.(all_mod_values)
+fixed_mods = .!varied_mods
 fixed_mods_dict = Dict(name => mods[name] for name in all_mod_names[fixed_mods])
 
 mod_names = all_mod_names[varied_mods] |> sort
@@ -23,6 +28,9 @@ classifications_A = NamedTuple{Tuple(classification_names)}([mod_array(Bool) for
 
 for (this_mod, this_result) in MultiDBRowIter(mdb)
     exec_classification = this_result[:wave_properties]
+    if exec_classification.has_propagation == true
+        @show "HEI"
+    end
     classifications_A_idx = this_mod[mod_names]
     if exec_classification === missing
         for name in classification_names
