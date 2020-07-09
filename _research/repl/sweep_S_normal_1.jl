00df74983e5889004ea20ebc6aafa4d51632521e
Graham Smith <grahams@uchicago.edu>
2020-07-08 23:11:26 -0500

    sweep modifications

--- _research/repl/sweep_S_normal_1.jl
+++ _research/repl/sweep_S_normal_1.jl
@@ -3,8 +3,9 @@ based_on_example(; example_name="reduced_line_dos_effectively_sigmoid",
                                        "Sie=14.0:16.0:320.0", "Sii=14.0:24.0:600.0",
                                        "firing_θE=6.0", "firing_θI=7.0",
                                        "blocking_θE=25.0", "blocking_θI=25.0",
-                                       "n_lattice=512", "x_lattice=1400",
-                                       "step_reduction=nothing"])
+                                       #"n_lattice=512", "x_lattice=1400.0",
+                                       "save_everystep=false"])#,
+                                       #"step_reduction=nothing"])
 # FIXME step_reduction is broken so reverting to "nothing"
 # This may be fine since I'm only saving the global reduction anyway
 # However it will limit the maximum size simulation
