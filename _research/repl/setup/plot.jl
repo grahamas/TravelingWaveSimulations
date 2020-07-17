d94f4e41c99d42f929d13e0eea5b978c2e3fb880
Graham Smith <grahamas@gmail.com>
2020-07-17 14:33:55 -0500

    [wip] too long names lead to problems

--- _research/repl/setup/plot.jl
+++ _research/repl/setup/plot.jl
@@ -1,4 +1,4 @@
 
-using Makie, MakieLayout, AbstractPlotting
+using Makie, MakieLayout, AbstractPlotting, StatsMakie
 
 AbstractPlotting.convert_arguments(P::AbstractPlotting.PointBased, nt::NamedTuple{names}) where names = AbstractPlotting.convert_arguments(P, [names...], [values(nt)...])
