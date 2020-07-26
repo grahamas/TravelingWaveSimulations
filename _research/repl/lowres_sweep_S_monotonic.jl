d50ea83571ca74cbac210070f7e1460c4c2997de
Graham Smith <grahamas@gmail.com>
2020-07-26 18:57:49 -0500

    Lowres test of more commensurate Aii

---
+++ _research/repl/lowres_sweep_S_monotonic.jl
@@ -0,0 +1,14 @@
+iterate_prototype("ring_monotonic", 
+                        ["See=14.0:24.0:120.0", "Sei=14.0:24.0:120.0",
+                         "Sie=14.0:24.0:120.0", "Sii=14.0:24.0:120.0",
+                         "Aii=50.0",
+                         "θE=6.0", 
+                         "θI=7.0",
+                         "save_everystep=false",
+						 "velocity_threshold=1e-6",
+						 "n_traveling_frames_threshold=60"],
+                        experiment_name="report2_S_sweep",
+                        data_root=data_root)
+# FIXME step_reduction is broken so reverting to "nothing"
+# This may be fine since I'm only saving the global reduction anyway
+# However it will limit the maximum size simulation
