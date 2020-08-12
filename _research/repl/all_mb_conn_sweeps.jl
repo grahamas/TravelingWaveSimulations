4201f87faa9aa58bfa314ecb06a76d79e83b1f42
Graham Smith <grahamas@gmail.com>
2020-08-12 14:53:14 -0500

    helper to call all monotonic/blocking sweeps (standard case)

---
+++ _research/repl/all_mb_conn_sweeps.jl
@@ -0,0 +1,5 @@
+
+include(joinpath(@__DIR__, "sweep_A_monotonic.jl"))
+include(joinpath(@__DIR__, "sweep_S_monotonic.jl"))
+include(joinpath(@__DIR__, "sweep_A_blocking.jl"))
+include(joinpath(@__DIR__, "sweep_S_blocking.jl"))
