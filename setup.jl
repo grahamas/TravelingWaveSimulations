97571db82433800ede08bd74699c5fb27a1e4f3f
Graham Smith <grahamas@gmail.com>
2020-08-31 15:57:38 -0500

    Add plotting packages to update and setup scripts

--- setup.jl
+++ setup.jl
@@ -2,41 +2,41 @@ using Pkg
 
 rel_path(dirs...) = joinpath(@__DIR__, dirs...)
 git_pull(path) = run(`git -C $(path) pull`)
+function init_julia_git(path)
+    Pkg.activate(path)
+    Pkg.update()
+end
+function init_julia_git(path, dev_dep_paths)
+    Pkg.activate(path)
+    Pkg.develop([PackageSpec(path=x) for x in dev_dep_paths])
+    Pkg.update()
+end
 
 s73_path = rel_path("..", "Simulation73")
 nm_path = rel_path("..", "NeuralModels")
 wcm_path = rel_path("..", "WilsonCowanModel")
 pde_path = rel_path("..", "FindPDE")
+s73_plot_path = rel_path("..", "Simulation73Plotting")
+tws_plot_path = rel_path("..", "TravelingWaveSimulationsPlotting")
 tws_path = rel_path(".")
 
-for path in [s73_path, nm_path, wcm_path, pde_path]
+all_paths = [s73_path, nm_path, wcm_path, pde_path, tws_path, s73_plot_path, tws_plot_path]
+
+for path in all_paths
 	if !ispath(path)
 		run(`git clone git@github.com:grahamas/$(basename(path)).git $(path)`)
 	end
 end
 
-git_pull.([s73_path, nm_path, wcm_path, pde_path, tws_path])
+git_pull.(all_paths)
 
-Pkg.activate(pde_path)
-Pkg.update()
-
-Pkg.activate(s73_path)
-Pkg.update()
-
-Pkg.activate(nm_path)
-Pkg.develop(PackageSpec(path=s73_path))
-Pkg.update()
-
-Pkg.activate(wcm_path)
-Pkg.develop([PackageSpec(path=x) for x in [s73_path, nm_path]])
-Pkg.update()
-
-Pkg.activate(tws_path)
-Pkg.develop([PackageSpec(path=x) for x in [s73_path, nm_path, wcm_path, pde_path]])
-Pkg.update()
-
-Pkg.activate(rel_path("_research"))
-Pkg.develop([PackageSpec(path=x) for x in [s73_path, nm_path, wcm_path, pde_path, tws_path]])
-Pkg.update()
+init_julia_git(pde_path)
+init_julia_git(s73_path)
+init_julia_git(nm_path, [s73_path])
+init_julia_git(wcm_path, [s73_path, nm_path])
+init_julia_git(tws_path, [s73_path, nm_path, wcm_path])
+init_julia_git(s73_plot_path, [s73_path])
+init_julia_git(tws_plot_path, [s73_plot_path, s73_path, nm_path, wcm_path, tws_path])
+init_julia_git(rel_path("_research"), [s73_path, nm_path, wcm_path, pde_path, tws_path, s73_plot_path, tws_plot_path])
 
 Pkg.build()
