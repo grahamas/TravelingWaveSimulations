a26b80608c6edf0728dbd04376badb65ccf8a7f5
Graham Smith <grahamas@gmail.com>
2020-10-24 00:08:12 -0500

    Resolve upstream dep conflict -- local tests pass

--- src/TravelingWaveSimulations.jl
+++ src/TravelingWaveSimulations.jl
@@ -7,7 +7,7 @@ using Lazy
 using Dates
 using Logging
 using Simulation73, NeuralModels, WilsonCowanModel
-using DifferentialEquations, DiffEqOperators
+using OrdinaryDiffEq, DiffEqBase, DiffEqOperators
 using ArgParse
 using IterTools
 using Statistics, LinearAlgebra
