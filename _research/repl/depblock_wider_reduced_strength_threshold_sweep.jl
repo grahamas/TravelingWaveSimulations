e9a7a669fe72912b97fd4e72386b115480f9118e
Graham Smith <grahamas@gmail.com>
2020-11-29 11:55:45 -0700

    Wider sweeps

--- _research/repl/depblock_wider_reduced_strength_threshold_sweep.jl
+++ _research/repl/depblock_wider_reduced_strength_threshold_sweep.jl
@@ -1,4 +1,4 @@
-iterate_prototype("ring_blocking",
+iterate_prototype("ring_normed_blocking",
                         ["Aee=40.0:48.0:350.0", "Aei=20.0:48.0:350.0",
                          "Aie=15.0:48.0:250.0", "Aii=1.0:60.0:250.0",
                          "θE=6.0", "firing_θI=7.0",
