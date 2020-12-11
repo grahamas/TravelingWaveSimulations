0bedb9c1f42a6f68898b54c2474dd6be769fd1ee
Graham Smith <grahamas@gmail.com>
2020-12-11 12:57:39 -0700

    Rename sweep scripts

--- scripts/depblock_sweeps/depblock_super_reduced_threshold_sweep.jl
+++ scripts/depblock_sweeps/depblock_super_reduced_threshold_sweep.jl
@@ -1,8 +1,8 @@
-iterate_prototype("ring_blocking",
+iterate_prototype("ring_normed_blocking",
                         ["Aee=40.0:100.0:250.0", "Aei=20.0:100.0:250.0",
                          "Aie=15.0:100.0:250.0", "Aii=1.0:100.0:250.0",
                          "θE=6.0", "firing_θI=7.0",
                          "blocking_θI=9.0:1.0:20.0",
                          "x_lattice=2800.0", "n_lattice=512"],
-                        experiment_name="reduced_depthreshold_A",
+                        experiment_name="super_reduced_depthreshold_A",
                         data_root=data_root)
