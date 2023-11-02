3260dfcba527eb325f969dc19b948f36e0edd2e7
Graham Smith <grahamas@gmail.com>
2023-11-01 22:33:11 -0700

    Remove deprecated DiffEqOperators (unused)

--- src/TravelingWaveSimulations.jl
+++ src/TravelingWaveSimulations.jl
@@ -7,7 +7,7 @@ using Lazy
 using Dates
 using Logging
 using Simulation73, NeuralModels, WilsonCowanModel
-using DiffEqBase, DiffEqOperators, OrdinaryDiffEq
+using DiffEqBase, OrdinaryDiffEq
 using ArgParse
 using IterTools
 using Statistics, LinearAlgebra
